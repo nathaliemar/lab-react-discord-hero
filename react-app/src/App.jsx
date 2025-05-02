eead5bd36dedf77c9b7fd89e74d30d725d3e4b3a
nathaliemar <n.marten.95@gmail.com>
2025-05-02 15:34:02 +0200

    done

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
         <h1>IMAGINE A PLACE...</h1>
         <p>
           ...where you can belong to a school club, a gaming group, or a
-          worldwide art community. Wehre just a handful of friends can spend
-          time together. A place that mkes it easy to talk every day and hang
+          worldwide art community. Where just a handful of friends can spend
+          time together. A place that makes it easy to talk every day and hang
           out more often.
         </p>
       </div>
